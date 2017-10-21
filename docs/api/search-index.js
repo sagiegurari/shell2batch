7345dab5a8ec770e8872f39e9760ca97a4fef781
sagie gur ari <sagiegurari@gmail.com>
2017-10-21 14:28:29 +0000

    publish

---
+++ docs/api/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["shell2batch"] = {"doc":"shell2batch","items":[[5,"convert","shell2batch","Converts the provided shell script and returns the windows batch script text.",null,{"inputs":[{"name":"str"}],"output":{"name":"string"}}]],"paths":[]};
+initSearch(searchIndex);
---
+++ docs/api/shell2batch/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["convert","Converts the provided shell script and returns the windows batch script text."]]});
