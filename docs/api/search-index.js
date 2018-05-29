ce744877b8b15557cde18bf1f1cdddbfcd9dc759
sagie gur ari <sagiegurari@gmail.com>
2018-05-29 23:17:07 +0300

    upgrade rust version

--- docs/api/search-index.js
+++ docs/api/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex["shell2batch"] = {"doc":"shell2batch","items":[[5,"convert","shell2batch","Converts the provided shell script and returns the windows batch script text.",null,{"inputs":[{"name":"str"}],"output":{"name":"string"}}]],"paths":[]};
+searchIndex["shell2batch"] = {"doc":"shell2batch","items":[[5,"convert","shell2batch","Converts the provided shell script and returns the windows batch script text.",null,{"i":[{"n":"str"}],"o":{"n":"string"}}]],"paths":[]};
 initSearch(searchIndex);
