f3313c5faf9542083b22845169f72751c34c6a65
sagie gur ari <sagiegurari@gmail.com>
2018-08-10 00:07:23 +0300

    update lint rules

--- docs/api/search-index.js
+++ docs/api/search-index.js
@@ -1,3 +1,3 @@
-var searchIndex = {};
-searchIndex["shell2batch"] = {"doc":"shell2batch","items":[[5,"convert","shell2batch","Converts the provided shell script and returns the windows batch script text.",null,{"i":[{"n":"str"}],"o":{"n":"string"}}]],"paths":[]};
+var N = null;var searchIndex = {};
+searchIndex["shell2batch"]={"doc":"shell2batch","items":[[5,"convert","shell2batch","Converts the provided shell script and returns the windows batch script text.",N,[[["str"]],["string"]]]],"paths":[]};
 initSearch(searchIndex);
