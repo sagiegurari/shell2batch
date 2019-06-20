c3854bd8fed91169c6a40060538fa59ada41a3d5
roblabla <unfiltered@roblab.la>
2019-06-20 16:31:58 +0000

    Update bench and examples

--- examples/example.rs
+++ examples/example.rs
@@ -28,7 +28,7 @@ set FILE1=file1
 set FILE2=file2
 
 @REM this is some test code
-xcopy %FILE1% %FILE2%
+copy %FILE1% %FILE2%
 
 @REM another
 move file2 file3
@@ -36,7 +36,7 @@ move file2 file3
 set MY_DIR=directory
 
 @REM flags are supported
-del /Q %MY_DIR%
+rmdir /S /Q %MY_DIR%
 
 set MY_DIR=
 "#
